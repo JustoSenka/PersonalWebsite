5a2aa89f950c684533de0d3f4d698f1b2b655de5
Justas <glodjus@gmail.com>
2020-05-09 14:18:16 +0200

    Adding modal images to projects page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,23 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+/* MODAL IMAGES */
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modal = document.getElementById("modal");
+var modalContent = document.getElementById("modal-content");
+var modalCaption = document.getElementById("modal-caption");
+
+// When the user clicks on <span> (x), close the modal
+var modalClose = document.getElementById("modal-close");
+modalClose.onclick = function () {
+    modal.style.display = "none";
+}
+
+$(function () {
+    $('.modal-img').on('click', function (e) {
+        modal.style.display = "block";
+        modalContent.src = this.src;
+        modalCaption.innerHTML = this.alt;
+    });
+});
